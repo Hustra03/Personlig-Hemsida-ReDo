ed55416d2207140d3cee0c241103514cd17862dc
Hustra03 <you@example.com>
2021-04-14 11:04:38 +0200

    Skapat Javascript + skapat variabler

---
+++ script.js
@@ -0,0 +1,19 @@
+let bild1= <img src="Bilder/IMG_1826.JPG" alt="En bild tagen vid svartsjö slottsparken, på insjön under hösten, och denna är nedsjunken i en fyrkantig avplanad grop, och marken är täckt av löven som fallit från träden." class="bild"/>;
+let text1= "Denna bild är tagen vid svartsjöslottsparken, vilket är en park som ligger relativt nära till där jag bor."
+
+let bild2=<img src="Bilder/IMG_1970.JPG" alt="En bild tagen i en skog nära till där jag bor, också på en insjö, men denna bild är tagen på vintern. Vattnet är helt fruset, och man ser fotspår från olika djur i tunn snö som delvis täcker isen, och i bakgrunden ses ett mindre berg. " class="bild" />
+let text2= "Denna bild är tagen i en skog i närheten till där jag bor, där ett före detta stenbrott nu är fyllt med vatten."
+
+let bild3= <img src="Bilder/IMG_2203.JPG" alt="En bild på min tomt. Vi ser en del av gräsmattan på den övre delen av tomten, och på den nedre delen vi en studsmatta och ett hallonland. I bakgrunden ses en skjul, och bortom detta en skog" class="bild" />
+let text3= "Denna bild är tagen från min tomt, och visar den nedre delen av tomten samt åkrarna och skogen som ligger bortom denna. "
+
+let bild4=<img src="Bilder/IMG_2269.JPG" alt="En bild på ett träd, mer specifikt en gran, som står på ett berg och i bakgrunden ses ett flertal andra liknande träd." width="33%" height="33%" />
+let text4="Denna bild är tagen på ett naturreservat som ligger i närheten till där jag bor, mer specifikt på en kulle där. "
+
+let bild5=<img src="Bilder/IMG_2309.JPG" alt="Liknande motiv som bild 3, men denna är tagen vid ett annat tillfälle och från en annan vinkel. Vi ser en växt i förgrunden, och i bakgrunden ses en höbal-bil samt en åker täkt av nyklippt gräs. " width="33%" height="33%" />
+let text5="Denna bild är, likt den tredje, också tagen från min tomt, och visar den nedre delen samt det som är bortom. Den är nyare, även om båda bilderna är ett par år gamla."
+
+let bild6=<img src="Bilder/IMG_1629.JPG" alt="Bilden är tagen vid en badplats, och i fokus är en liten fågel som sitter på bryggan. Vi ser även bojar och en segelbåt i bakgrunden" width="33%" height="33%" />
+let text6="Denna bild är tagen vid den lokala badplatsen, i förgrunden ser bryggan, och på denna sitter en liten fågel, medans i bakgrunden så ser vi bojar och en segelbåt. "
+
+
