b77d3977e78a43ceff9e1abf9b792d0fbaedf95a
Hustra03 <you@example.com>
2021-05-12 10:09:18 +0200

    Fixat scripten

--- minÅlder.js
+++ minÅlder.js
@@ -5,8 +5,8 @@ var days = hours * 24;
 var years = days * 365;
 var temp = 0;
 var day = 0;
-var temp2=0;
-var hour=0;
+var temp2 = 0;
+var hour = 0;
 
 function MinÅlder() {
   timerPosition.innerHTML = "";
@@ -16,13 +16,24 @@ function MinÅlder() {
   t = t / years;
   temp = t;
   t = Math.round(t);
-  temp -= t;
-  temp2=temp*365;
-  day = Math.round(temp*365);
-  temp2 -= day;
-  hour = Math.round(temp2*24)
+  if (t < temp) {
+    temp -= t;
+  } else {
+    t -= 1;
+    temp -= t;
+  }
+  temp2 = temp * 365;
+  day = Math.round(temp * 365);
+  if (day < temp2) {
+    temp2 -= day;
+  } else {
+    day -= 1;
+    temp2 -= day;
+  }
+  hour = Math.round(temp2 * 24);
   let p = document.createElement("p");
-  p.innerText = "Jag är " + t + " år, " + day + " dagar och "+hour +" timmar gammal";
+  p.innerText =
+    "Jag är " + t + " år, " + day + " dagar och " + hour + " timmar gammal";
   timerPosition.append(p);
 }
 MinÅlder();
